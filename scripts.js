d1f93068e350401e25183f7d744fca91a701d21d
aterentyev <andrei@solarisoffgrid.com>
2019-08-12 11:52:48 +0200

    locations

--- scripts.js
+++ scripts.js
@@ -21,21 +21,21 @@ request.onload = function () {
   console.log(request.status);
   if (request.status >= 200 && request.status < 400) {
     debugger;
-    //   // data.forEach(movie => {
-    //   const card = document.createElement('div');
-    //   card.setAttribute('class', 'card');
+    data.forEach(locations.id => {
+    const card = document.createElement('div');
+    card.setAttribute('class', 'card');
 
-    //   const h1 = document.createElement('h1');
-    //   h1.textContent = movie.title;
+    const h1 = document.createElement('h1');
+    h1.textContent = id;
 
     //   const p = document.createElement('p');
     //   movie.description = movie.description.substring(0, 300);
     //   p.textContent = `${movie.description}...`;
 
-    //   container.appendChild(card);
-    //   card.appendChild(h1);
-    //   card.appendChild(p);
-    //  });
+    container.appendChild(card);
+    card.appendChild(h1);
+    card.appendChild(p);
+   });
   }
   else {
     const errorMessage = document.createElement('marquee');
